4e1eb9de39999210cf0c790684e9e4898cb84fc3
asop321 <asop321@gmail.com>
2022-04-16 02:30:55 +0900

    첫번째 커밋

---
+++ js/bxSlider.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.gallery').bxSlider({
+     
+      slideWidth: 600,
+      slideWidth:200,
+      minSlides:1,
+      maxSlides:4,
+      slideMargin:30,
+      moveSlides:4,
+      pager:false,
+
+    });
+})
+ 
